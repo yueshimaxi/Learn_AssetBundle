08f732aab1f6c5277140d16dfd9613279f2daca1
XUZHIPENG <1071556447@qq.com>
2020-06-13 23:41:34 +0800

    第一次提交 有build和load功能

---
+++ Assets/Editor/BuildAB.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class BuildAB : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    [MenuItem("Assets/Bulid AssetBundle")]
+    static void BuildAssetBundle()
+    {
+        //创建目录
+        string abPath = "Assets/AssetBundles";
+        if (!Directory.Exists(abPath))
+        {
+            Directory.CreateDirectory(abPath);
+        }
+        //打包 os/win
+#if UNITY_STANDALONE_WIN
+        BuildPipeline.BuildAssetBundles(abPath ,BuildAssetBundleOptions.None   , BuildTarget.StandaloneWindows64);
+#elif UNITY_STANDALONE_OSX
+				BuildPipeline.BuildAssetBundles(abPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneOSX);
+#endif
+
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+public class test : MonoBehaviour
+{
+  public  GameObject wallPrefab;
+  public  GameObject wallPrefab1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown  (KeyCode.Space))
+        {
+            StartCoroutine(getobject());
+        }
+    if (Input.GetKeyDown  (KeyCode.W ))
+        {
+            StartCoroutine(getobject1());
+        }
+    }
+
+    IEnumerator getobject()
+    {
+        if (wallPrefab != null)
+        {
+            Instantiate(wallPrefab);
+        }
+        else
+        {
+            string _abPath = Application.dataPath + "/AssetBundles/model";
+            AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(_abPath));
+            yield return request;
+            AssetBundle ab = request.assetBundle;
+            wallPrefab = ab.LoadAsset<GameObject>("Sphere.prefab");
+       
+                Instantiate(wallPrefab);
+
+         
+        }
+      
+      
+    }
+ IEnumerator getobject1()
+    {
+        if (wallPrefab1 != null)
+        {
+            Instantiate(wallPrefab1);
+        }
+        else
+        {
+            string _abPath = Application.dataPath + "/AssetBundles/model1.u3d";
+            AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(_abPath));
+            yield return request;
+            AssetBundle ab = request.assetBundle;
+            wallPrefab1 = ab.LoadAsset<GameObject>("Cube.prefab");
+            Instantiate(wallPrefab);
+        }
+      
+      
+    }
+
+}
